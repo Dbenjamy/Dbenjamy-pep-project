77c32f1aca7dbf0d28ef26caa3e25fbe54d0a092
Benjamin Davis <ben.yj.davis@gmail.com>
2025-02-05 17:39:57 +0000

    starting messagedoa

---
+++ src/main/java/DAO/MessageDOA.java
@@ -0,0 +1,38 @@
+package DAO;
+
+import Util.ConnectionUtil;
+import Model.Message;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MessageDOA {
+        
+    public List<Message> getAllMessages(){
+        Connection connection = ConnectionUtil.getConnection();
+        List<Message> messages = new ArrayList<>();
+        
+        try {
+            String sql = "SELECT * FROM message;";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet rs = preparedStatement.executeQuery();        
+            
+            while (rs.next()) {
+                messages.add(new Message(
+                   rs.getInt("message_id"),
+                   rs.getInt("posted_by"),
+                   rs.getString("message_text"),
+                   rs.getLong("time_posted_epoch")
+                ));
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return messages;
+    }
+}
