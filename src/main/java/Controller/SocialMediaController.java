a83d19dfc767bd762743a4472f67b4a6f1812593
Benjamin Davis <ben.yj.davis@gmail.com>
2025-02-06 23:19:13 +0000

    Message creation and retrival

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -2,7 +2,13 @@ package Controller;
 
 import java.util.List;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import DAO.AccountDOA;
 import DAO.MessageDOA;
+import Model.Account;
 import Model.Message;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
@@ -18,13 +24,22 @@ public class SocialMediaController {
      * suite must receive a Javalin object from this method.
      * @return a Javalin app object which defines the behavior of the Javalin controller.
      */
+    private AccountDOA accountDOA;
     private MessageDOA messageDOA;
+    private ObjectMapper objectMapper;
 
     public Javalin startAPI() {
+        accountDOA = new AccountDOA();
         messageDOA = new MessageDOA();
+        objectMapper = new ObjectMapper();
         Javalin app = Javalin.create();
+
+        app.post("/register", this::registerAccount);
+        app.post("/login", this::accountLogin);
+        app.post("/messages",this::createMessage);
         app.get("example-endpoint", this::exampleHandler);
         app.get("/messages", this::getAllMessages);
+        app.get("/messages/{id}", this::getMessageById);
         return app;
     }
 
@@ -36,31 +51,108 @@ public class SocialMediaController {
         context.json("sample text");
     }
 
+    private void registerAccount(Context context) {
+        String jsonString = context.body();
+        Account account;
+        List<Account> accounts;
+        try {
+            account = objectMapper.readValue(jsonString, Account.class);
+            accounts = accountDOA.registerAccount(
+                account.getUsername(), 
+                account.getPassword());
+            if (accounts.size() > 0) {
+                context.json(accounts.get(0));
+            } else {
+                context.status(400);
+                context.json("");
+            }
+            
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void accountLogin(Context context) {
+        String jsonString = context.body();
+        Account account;
+        List<Account> accounts;
+        try {
+            account = objectMapper.readValue(jsonString, Account.class);
+            accounts = accountDOA.accountLogin(
+                account.getUsername(),
+                account.getPassword());
+            if (accounts.size() > 0) {
+                context.json(accounts.get(0));
+            } else {
+                context.status(401);
+                context.json("");
+            }
+            
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createMessage(Context context) {
+        String jsonString = context.body();
+        boolean success = false;
+        try {
+            Message message = objectMapper.readValue(jsonString, Message.class);
+            int posted_by = message.getPosted_by();
+            String message_text = message.getMessage_text();
+            long time_posted_epoch = message.getTime_posted_epoch();
+
+            if (message_text.length() > 255
+                    || message_text == ""
+                    || accountDOA.getAccountById(posted_by).size() == 0) {
+                success = messageDOA.createMessage(
+                    posted_by,
+                    message_text,
+                    time_posted_epoch);
+            }
+
+            if (success) {
+                context.json(messageDOA.getMessageByText(message.getMessage_text()));
+            } else {
+                context.status(400);
+                context.json("");
+            }
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // private String buildMessageJson(Message messageObj) {
+    //     String jsonString = "{\n        "
+    //         + "\"message_id\":%d,\n        "
+    //         + "\"posted_by\":%d,\n        "
+    //         + "\"message_text\":%s,\n        "
+    //         + "\"time_posted_epoch\":%f,\n"
+    //         + "    }";
+        
+    //     String message = String.format(jsonString,
+    //         messageObj.getMessage_id(),
+    //         messageObj.getPosted_by(),
+    //         messageObj.getMessage_text(),
+    //         messageObj.getTime_posted_epoch());
+    //     return message;
+    // }
     private void getAllMessages(Context context) {
         List<Message> messages = messageDOA.getAllMessages();
-        String jsonString = "";
-        if (messages.size() > 1) {
-            jsonString = jsonString + "{\n    ";
-            for (int i = 0; i < messages.size(); i++) {
-                Message messageObj = messages.get(i);
-                String message = "{\n        "
-                    + "\"message_id\":%d,\n        "
-                    + "\"posted_by\":%d,\n        "
-                    + "\"message_text\":%s,\n        "
-                    + "\"time_posted_epoch\":%f,\n"
-                    + "    },\n{    ";
-                message = String.format(jsonString,
-                    messageObj.getMessage_id(),
-                    messageObj.getPosted_by(),
-                    messageObj.getMessage_text(),
-                    messageObj.getTime_posted_epoch());
-                jsonString = jsonString + message;
-            }
-            jsonString = jsonString.substring(0, jsonString.length()-7);
-            jsonString = jsonString + "\n}";
-        }
         context.status(200);
         context.json(messages);
     }
 
+    private void getMessageById(Context context) {
+        int id = Integer.valueOf(context.pathParam("id"));
+        List<Message> messages = messageDOA.getMessageById(id);
+        context.status(200);
+        if (messages.size() > 0) {
+            context.json(messages.get(0));
+        } else {
+            context.json("");
+        }
+    }
+
+    
 }
--- src/main/java/DAO/MessageDOA.java
+++ src/main/java/DAO/MessageDOA.java
@@ -11,6 +11,50 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MessageDOA {
+
+    
+
+    public boolean createMessage(int posted_by, String message_text, Long time_posted_epoch) {
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+            String sql = "INSERT INTO message (posted_by, message_text, time_posted_epoch)"
+                + " VALUES (?, ?, ?);";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, posted_by);
+            preparedStatement.setString(2, message_text);
+            preparedStatement.setFloat(3, time_posted_epoch);
+
+            int linesChanged = preparedStatement.executeUpdate();
+            if (linesChanged == 1) {
+                return true;
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+
+    public List<Message> getMessageByText(String text) {
+        Connection connection = ConnectionUtil.getConnection();
+        List<Message> messages = new ArrayList<>();
+        try {
+            String sql = "SELECT * FROM message WHERE message_text = ?;";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, text);
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                messages.add(new Message(
+                    rs.getInt("message_id"),
+                    rs.getInt("posted_by"),
+                    rs.getString("message_text"),
+                    rs.getLong("time_posted_epoch")
+                ));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return messages;
+    }
         
     public List<Message> getAllMessages(){
         Connection connection = ConnectionUtil.getConnection();
@@ -36,28 +80,27 @@ public class MessageDOA {
         return messages;
     }
 
-    public Message getMessageById(int id){
+    public List<Message> getMessageById(int id){
         Connection connection = ConnectionUtil.getConnection();
+        List<Message> messages = new ArrayList<>();
+
         try {
             String sql = "SELECT * FROM message WHERE message_id = ?;";
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             preparedStatement.setInt(1, id);
             ResultSet rs = preparedStatement.executeQuery();
-
-            while (rs.next()){
-                Message message = new Message(
-                    rs.getInt("message_id"),
-                    rs.getInt("posted_by"),
-                    rs.getString("message_text"),
-                    rs.getLong("time_posted_epoc")
-                );
-                return message;
+            while (rs.next()) {
+                messages.add(new Message(
+                   rs.getInt("message_id"),
+                   rs.getInt("posted_by"),
+                   rs.getString("message_text"),
+                   rs.getLong("time_posted_epoch")
+                ));
             }
 
-
-        } catch (Exception e) {
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
-        return null;
+        return messages;
     }
 }
